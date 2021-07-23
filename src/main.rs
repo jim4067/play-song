257c157501fe83b9b9866618cc3f66cc66e6122d
jim4067 <jimmyimpulse2@gmail.com>
2021-07-23 21:10:47 +0300

    :tada: :construction: main function :tada: :construction:

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
+extern crate id3;
 extern crate rodio;
 extern crate walkdir;
-extern crate id3; 
 
 mod app;
 mod playback;
 mod songs;
 
 fn main() {
-    // let thread_one = std::thread::spawn(move || {
-    //     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    //     let sink = rodio::Sink::try_new(&handle).unwrap();
-    //     let file = File::open("/media/jim_4067/52AE2FD4AE2FAF81/squidward tenticles/Sabrina Carpenter – Singular Act 1 (2018)/Prfct.mp3").unwrap();
-    //     sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-    //     sink.sleep_until_end();
-
-    //     play_fn()
-    // });
     playback::play_fn();
 }
-
-//how to structure this thing
-//the fn to play the songs
-//the fn to list the songs
-//the fn for the metadata
