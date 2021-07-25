9c5d9abe9b3a94dc0969d81448e502843d8a68de
jim4067 <jimmyimpulse2@gmail.com>
2021-07-25 17:53:26 +0300

    :construction: :tada: added a songs struct, to store mp3 and m4a songs :tada: :construction:

--- src/songs.rs
+++ src/songs.rs
@@ -1,30 +1,42 @@
 use std::io;
 use walkdir::WalkDir;
 
-pub fn list_songs() -> io::Result<Vec<String>> {
-    let mut files = Vec::new();
-    for entry in WalkDir::new(".").min_depth(1).max_depth(3) {
-        let entry = entry.unwrap();
-        let path = entry.path().to_owned();
-        files.push(path);
-    }
-
-    //cleaning the file names - removing path details
-    let file_names: Vec<String>;
-    file_names = files
-        .iter()
-        .map(|res| format!("{:?}", res.to_str().unwrap()))
-        .collect();
-
-    let mut songs = Vec::new();
-    for song in file_names {
-        let song: String = song[3..song.len() - 1].to_string();
-        if song.trim().ends_with(".mp3") {
-            songs.push(song);
-        }
-        //Issue an error here if no mp3 files are found
-    }
-    Ok(songs)
+#[derive(Debug)]
+pub struct Songs {
+    mp3_songs: Vec<String>,
+    m4a_songs: Vec<String>,
 }
 
-//lists the songs found in the directory
+impl Songs {
+    pub fn list_songs() -> io::Result<Songs> {
+        let mut files = Vec::new();
+        for entry in WalkDir::new(".").min_depth(1).max_depth(3) {
+            let entry = entry.unwrap();
+            let path = entry.path().to_owned();
+            files.push(path);
+        }
+        //cleaning the file names - removing path details
+        let file_names: Vec<String>;
+        file_names = files
+            .iter()
+            .map(|res| format!("{:?}", res.to_str().unwrap()))
+            .collect();
+        let mut mp3_songs = Vec::new();
+        let mut m4a_songs = Vec::new();
+        for song in file_names {
+            //other file formats can be added here too
+            let song: String = song[3..song.len() - 1].to_string();
+            if song.trim().ends_with(".mp3") {
+                mp3_songs.push(song);
+            } else if song.trim().ends_with(".m4a") {
+                m4a_songs.push(song);
+            }
+            //Issue an error here if no mp3 files are found
+        }
+        Ok(Self {
+            mp3_songs,
+            m4a_songs,
+        })
+    }
+}
+
